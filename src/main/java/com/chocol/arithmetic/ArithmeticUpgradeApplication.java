f94b442241016b94097e6456640035fd85a161fe
PowerChen <314159715@qq.com>
2021-06-07 23:19:11 +0800

    init repository

---
+++ src/main/java/com/chocol/arithmetic/ArithmeticUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.chocol.arithmetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmeticUpgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArithmeticUpgradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chocol/arithmetic/ArithmeticUpgradeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.chocol.arithmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmeticUpgradeApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
