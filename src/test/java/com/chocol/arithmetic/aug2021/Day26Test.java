ba3be7cc6aa3afecc258f550b9d21b6cf1d72299
powerchen <314159715@qq.com>
2021-08-30 10:02:56 +0800

    feat: 1588. 所有奇数长度子数组的和

---
+++ src/test/java/com/chocol/arithmetic/aug2021/Day26Test.java
@@ -0,0 +1,47 @@
+package com.chocol.arithmetic.aug2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description:
+ *
+ * @author 陈力
+ * @date 2021/6/8
+ * @since
+ */
+@Slf4j
+@SpringBootTest
+public class Day26Test {
+
+    @Test
+    public void test01() {
+        int[] nums = {1, 1, 1, 1, 1};
+        int[] result = runningSum(nums);
+        System.out.println();
+    }
+
+    /**
+     * 1480. 一维数组的动态和
+     *
+     * 给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。
+     * 
+     * 请返回 nums 的动态和。
+     * 
+     * 链接：https://leetcode-cn.com/problems/running-sum-of-1d-array
+     *
+     * @return
+     */
+    public int[] runningSum(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] + nums[i];
+        }
+
+        return ans;
+    }
+
+}
---
+++ src/test/java/com/chocol/arithmetic/aug2021/Day27Test.java
@@ -0,0 +1,62 @@
+package com.chocol.arithmetic.aug2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Description:
+ *
+ * @author 陈力
+ * @date 2021/6/8
+ * @since
+ */
+@Slf4j
+@SpringBootTest
+public class Day27Test {
+
+    @Test
+    public void test01() {
+        int[] arr = {1, 4, 2, 5, 3};
+        int result = sumOddLengthSubarrays(arr);
+        assert 58 == result;
+    }
+
+    /**
+     * 1588. 所有奇数长度子数组的和
+     * 
+     * 给你一个正整数数组arr，请你计算所有可能的奇数长度子数组的和。
+     * 
+     * 子数组 定义为原数组中的一个连续子序列。
+     * 
+     * 请你返回 arr中 所有奇数长度子数组的和 。
+     * 
+     * 1 <= arr.length <= 100
+     * 
+     * 1 <= arr[i] <= 1000
+     * 
+     * 链接：https://leetcode-cn.com/problems/sum-of-all-odd-length-subarrays
+     *
+     * @return
+     */
+    public int sumOddLengthSubarrays(int[] arr) {
+        // 双指针
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int left = i;
+            int right = left;
+            while (right < arr.length) {
+                if ((right - left) % 2 == 0) {
+                    for (int j = left; j <= right; j++) {
+                        ans += arr[j];
+                    }
+                }
+                right++;
+            }
+        }
+
+        return ans;
+    }
+
+}
